8512cb4738e0b46a681f636793af0b4004b96042
NwekeFavour <buchijunior90@gmail.com>
2025-11-09 17:13:19 +0100

    first commit

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -11,14 +11,14 @@ function HeroSection(props) {
             <div className=''>
                 <div className='flex justify-center items-center sm:my-40 sam md:my-40 md:mx-20 xl:my-32 sm:mx-8 mx-5 xl:mx-30'>
                     <div className='md:w-[797px]'>
-                        <div className='bg-white upc sm:ms-6 ms-5 md:ms-0 lg:ms-6 w-fit border-2 px-6 py-2 rounded-[20px] mb-2  lg:mb-0 md:mb-3'>
+                        <div className='bg-white  upc sm:ms-6 ms-5 md:ms-0 lg:ms-6 w-fit border-2 px-6 py-2 rounded-[20px] mb-2  lg:mb-0 md:mb-3'>
                             <p className="m-0 uppercase text-[#439642] text-[12px] font-bold!"><span className='text-[#242424]'>upcoming</span> new event 2026</p>
                         </div>
                         <div>
                             <p className="m-0 text-center text-white lg:text-[40px] md:text-[36px] leading-10 sm:text-[35px] text-[28px] font-bold sm:leading-[55px] tracking-[-1%]">Experience the Power of Innovation and Collaboration in Abuja</p>
                         </div>
                         <div className='mt-4'>
-                            <p className="m-0 mx-10 sm:mx-18 font-medium sm:text-[18px] text-white text-[16px] leading-[29px] text-center">Techstars Startup Week is a five-day, entrepreneur-led, community event held in cities across the world to showcase the local startup ecosystem through meetups, lectures, discussions, and celebrations.</p>
+                            <p className="m-0 mx-2 sm:mx-18 font-medium sm:text-[18px] text-white text-[16px] leading-[29px] text-center">Techstars Startup Week is a five-day, entrepreneur-led, community event held in cities across the world to showcase the local startup ecosystem through meetups, lectures, discussions, and celebrations.</p>
                         </div>
                         <div className='flex justify-center items-center my-10'>
                             <Button className="text-white lg:text-[20px] sm:text-[18px] md:text-[18px] hover:bg-[#132513] hover:text-[#ffff] px-8 text-[17px] md:px-12 sm:px-14 sm:py-7 lg:px-9 py-7 rounded-[20px] bg-[#439642]">Register Now</Button>
